649da81449acea5c55fee5913654d5021290a7f0
sharathkrishnegowda <mesharu45@gmail.com>
2023-04-15 13:25:15 +0530

    ToDo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 // Express setup
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // View engine
